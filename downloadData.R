9fd0050632db403c725abcc0410ab482ca2caad1
patrickschaffner <patrickschaffner@users.noreply.github.com>
2017-04-26 15:44:26 +0200

    Update downloadData.R

--- downloadData.R
+++ downloadData.R
@@ -3,6 +3,6 @@
 
 if (!file.exists('CSDMC2010_SPAM')) {
   download("http://csmining.org/index.php/spam-email-datasets-.html?file=tl_files/Project_Datasets/task2/CSDMC2010_SPAM.zip", dest="dataset.zip", mode="wb") 
-  unzip ("dataset.zip", exdir = "./")
+  unzip ("dataset.zip", exdir = ".")
   file.remove("dataset.zip")
 }
