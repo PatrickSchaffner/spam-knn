0480d86bf2a1303362d591fb21ae35e4dd11ae18
patrickschaffner <patrickschaffner@users.noreply.github.com>
2017-04-26 16:42:56 +0200

    Update downloadData.R

--- downloadData.R
+++ downloadData.R
@@ -5,4 +5,5 @@ if (!file.exists('CSDMC2010_SPAM')) {
   download("http://csmining.org/index.php/spam-email-datasets-.html?file=tl_files/Project_Datasets/task2/CSDMC2010_SPAM.zip", dest="dataset.zip", mode="wb") 
   unzip ("dataset.zip", exdir = ".")
   file.remove("dataset.zip")
+  file.remove("CSDMC2010_SPAM/CSDMC2010_SPAM/TRAINING/TRAIN_02402.eml") # This one is evil!
 }
